c00a31b7655da22aef3a75eb59a8c84f799e53f8
Anber Jamshed <anberjam@yahoo.com>
2020-09-17 21:13:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,12 +5,18 @@ def loop_message_five_times (message)
   end
 end
 
-def loop_message_n_times
-  
+def loop_message_n_times (message, int)
+  int.times do
+    puts message
+  end
 end
 
-def output_array
+def output_array (array)
+  array.each |element| do
+    puts element
+  end
 end
 
 def return_string_array
+  
 end
